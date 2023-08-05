4b8f59e20a21a63e1af663c160ff66b470204f12
aryarmdhn <aryaramadhanu.gmail.com>
2023-08-06 00:13:18 +0700

    portofolio artarmdhn

---
+++ dist/js/script.js
@@ -0,0 +1,20 @@
+//navbarfix
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+// burger
+const burger = document.querySelector('#burger');
+const navMenu = document.querySelector('#nav-menu');
+
+burger.addEventListener('click', function() {
+    burger.classList.toggle('burger-active');
+    navMenu.classList.toggle('hidden'); 
+});
